8ebd9ff5ed33f1b750ba6e9e71004b48cff6c2d2
Kevin <kevin@dendron.so>
2023-05-20 09:51:13 -0700

    add tags field

--- build_database.py
+++ build_database.py
@@ -1,26 +1,84 @@
-from datetime import timezone, datetime
-from typing import List
-import httpx
-import git
+from dataclasses import dataclass
 import os
 import pathlib
+import sys
+import time
+from datetime import datetime, timezone
+from typing import List
 from urllib.parse import urlencode
+
+import frontmatter
+import httpx
 import sqlite_utils
 from sqlite_utils.db import NotFoundError
-import time
-import frontmatter
-import sys
-
-import sys
 
 sys.path.append(".")
 from dendron_sdk.client import DendronClient
 from dendron_sdk.environment import FernApiEnvironment
+from pathlib import Path
 
 
 root = pathlib.Path(__file__).parent.resolve()
 DENDRON_CLIENT = DendronClient(environment=FernApiEnvironment.LOCAL)
 GITHUB_BASE_URL = "https://github.com/kevinslin/kevinweblog"
+
+# === Types
+
+@dataclass
+class Note:
+    filepath: Path
+    metadata: dict
+    content: str
+    title: str
+    body: str
+    id: str
+    htag: str
+    topic: str
+    created: str
+    updated: str
+    path: str
+    slug: str
+    url: str
+    tags: List[str]
+    path_slug: str
+
+    @staticmethod
+    def get_note(filepath: str):
+        filepath = Path(filepath)
+        fp = filepath.open()
+        metadata, content = frontmatter.parse(fp.read().strip())
+        title = metadata["title"]
+        body = content
+        id = metadata["id"]
+        htag = metadata.get("htag", "none")
+        tags = metadata.get("tags", [])
+        topic = htag
+        created = datetime.fromtimestamp(metadata["created"] / 1000).isoformat()
+        updated = datetime.fromtimestamp(metadata["updated"] / 1000).isoformat()
+        path = str(filepath.relative_to(root))
+        slug = filepath.stem
+        url = f"{GITHUB_BASE_URL}/blob/main/{path}"
+        path_slug = path.replace("/", "_")
+        return Note(
+            filepath=filepath,
+            metadata=metadata,
+            content=content,
+            title=title,
+            body=body,
+            id=id,
+            htag=htag,
+            topic=topic,
+            created=created,
+            updated=updated,
+            path=path,
+            slug=slug,
+            url=url,
+            tags=tags,
+            path_slug=path_slug
+        )
+
+
+#
 
 def render_md(body, path, record):
     retries = 0
@@ -57,61 +115,45 @@ def render_md(body, path, record):
             path, response.headers
         )
 
-
 def build_database(repo_path):
     db = sqlite_utils.Database(repo_path / "notes.db")
     table = db.table("note", pk="path")
     seen = []
     for filepath in root.glob("dendron/**/*.md"):
         print(filepath)
-        fp = filepath.open()
-        metadata, content = frontmatter.parse(fp.read().strip())
-        title = metadata["title"]
-        body = content
-        id = metadata["id"]
-        htag = metadata.get("htag", "none")
-        topic = htag
-        created = datetime.fromtimestamp(metadata["created"] / 1000).isoformat()
-        updated = datetime.fromtimestamp(metadata["updated"] / 1000).isoformat()
-        path = str(filepath.relative_to(root))
-        slug = filepath.stem
-        # TODO: update
-        url = f"{GITHUB_BASE_URL}/blob/main/{path}"
-        # Do we need to render the markdown?
-        path_slug = path.replace("/", "_")
+        note = Note.get_note(filepath)
         seen.append(id)
+
         try:
-            row = table.get(path_slug)
+            row = table.get(note.path_slug)
             previous_body = row["body"]
             previous_html = row["html"]
         except (NotFoundError, KeyError):
             previous_body = None
             previous_html = None
         record = {
-            "id": id,
-            "path": path_slug,
-            "slug": slug,
-            "topic": topic,
-            "htag": htag,
-            "title": title,
-            "url": url,
-            "body": body,
-            # TODO
-            "created_utc": created,
-            "created": created,
-            "updated_utc": updated,
-            "updated": updated,
+            "id": note.id,
+            "path": note.path_slug,
+            "slug": note.slug,
+            "topic": note.topic,
+            "htag": note.htag,
+            "title": note.title,
+            "url": note.url,
+            "body": note.body,
+            "tags": note.tags,
+            "created_utc": note.created,
+            "created": note.created,
+            "updated_utc": note.updated,
+            "updated": note.updated,
         }
-        # TODO: use dendron version
-        if (body != previous_body) or not previous_html:
-
-            # TODO: temporary until we get dendron version
-            render_md(body, path, record)
-            # response = DENDRON_CLIENT.markdown_render(request={"content": body})
+        if (note.body != previous_body) or not previous_html:
+            render_md(note.body, note.path, record)
+            # response = DENDRON_CLIENT.markdown_render(request={"content": note.body})
             # record["html"] = response.content
-            # print("Rendered HTML for {}".format(path))
+            # print("Rendered HTML for {}".format(note.path))
         with db.conn:
             table.upsert(record, alter=True)
+
 
     delete_removed_notes(db, seen)
     table.enable_fts(
